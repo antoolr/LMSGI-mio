2f3182f65ca8f433944eaeb9f71a15e185157aaa
antoolr <antoniolozanorodriguez13@gmail.com>
2026-02-17 12:46:04 +0100

    d

---
+++ portfolio/src/layout/MainLayout.tsx
@@ -0,0 +1,13 @@
+
+// este componente crea el front-end de l aaplicación
+
+import Header from "../Components/main/Header"
+
+// la parte pública de la aplicación
+function MainLayout () {
+    return (
+    <Header />
+    )
+}
+
+export default MainLayout
